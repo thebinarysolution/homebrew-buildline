2e55e6ca86e84f7417d5d72fd6f62e491383a980
Lakhwinder Singh <11435085+thebinarysolution@users.noreply.github.com>
2026-06-16 11:35:17 +0530

    Update Release

--- Formula/buildline.rb
+++ Formula/buildline.rb
@@ -1,9 +1,9 @@
 class Buildline < Formula
   desc "CI/CD for iOS apps — one config, app in the store"
   homepage "https://github.com/thebinarysolution/homebrew-buildline"
-  version "0.1.0"
-  url "https://github.com/thebinarysolution/homebrew-buildline/releases/download/v0.1.0/buildline_0.1.0_darwin_all.tar.gz"
-  sha256 "49527034f976dddccf025b99a88ebab2ec061ca1a31e7fbb14aea255a0fc95a7"
+  version "0.1.1"
+  url "https://github.com/thebinarysolution/homebrew-buildline/releases/download/v0.1.1/buildline_0.1.1_darwin_all.tar.gz"
+  sha256 "4ab43d8638e5dd0518855ec3b4401ac1927d3ff5c8a1b88112e215c1bfab2a37"
 
   depends_on :macos
 
